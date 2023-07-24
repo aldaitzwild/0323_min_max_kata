86e377a6071496389c6df9369ae4a6f7588faeea
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-07-24 09:24:40 +0200

    Add the correction

--- kata.php
+++ kata.php
@@ -7,3 +7,14 @@
  */
 
 $values = [162, 71, 89, 90, 138, 0, -48, -28, -16, 92, 166, 177, -49, -21, 132, 25, 103, 177, 127, 173];
+
+$min = $values[0];
+$max = $values[0];
+
+foreach($values as $value) {
+    if($min > $value) $min = $value;
+    if($max < $value) $max = $value;
+}
+
+echo "Valeur maximale : $max\n";
+echo "Valeur minimale : $min";
