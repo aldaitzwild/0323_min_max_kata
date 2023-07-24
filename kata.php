c7b4a9522dd316fefaaef87fff10d9b58b4f85c4
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-07-24 08:51:14 +0200

    Add the kata for the day

---
+++ kata.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Sans utiliser la moindre fonction php trouver la valuer maximum 
+ * et minimum du tableau suivant. Puis les afficher.
+ * 
+ */
+
+$values = [162, 71, 89, 90, 138, 0, -48, -28, -16, 92, 166, 177, -49, -21, 132, 25, 103, 177, 127, 173];
